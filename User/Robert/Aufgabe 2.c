07f8384619396528956864d4d2f49bf8f966cb7b
Ruhrpottpatriot <Ruhrpottpatriot@gmx.de>
2014-10-27 12:53:00 +0100

    Aufgaben gelöst und Abgabeordner erstellt
    Hab Aufgaben 2 & 3 gelöst. Aufgabe 1 stellen wir am besten analog auf
    einem Blatt dar. Flussdiagramme sind was blöd als txt.

---
+++ User/Robert/Aufgabe 2.c
@@ -0,0 +1,28 @@
+// Aufgabe 2.1
+int Addieren(int n)
+{
+	a = 0;
+	
+	for (i = 1; i <= n; i++)
+	{
+		a += i;
+	}
+	
+	return a;
+}
+
+// Aufgabe 2.2
+int AddierenQuadratzahl(int number)
+{
+	result = 0;
+	
+	for(i = 1; i <= number; i++)
+	{
+		int b = i^2;
+		
+		a = a + i;
+	}
+	
+	return a;
+}
+s
---
+++ User/Robert/Aufgabe 3.c
@@ -0,0 +1,27 @@
+﻿// Aufgabe 3.1
+bool TestForNegativeNumber(int numberToTest)
+{
+	if(n < 0)
+	{
+		return false;
+	}
+}
+
+// Aufgabe 3.2
+
+#include <math.h>
+
+bool PrimeTest(int numberToTest)
+{
+	int n = 2;
+
+	if (n > sqrt(numberToTest))
+	{
+		return true;
+	}
+
+	if(numberToTest % n == 0)
+	{
+		return false;
+	}
+}
